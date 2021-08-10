53eeb86e0071886c2976184a4cca99c61777a0c2
Nicolas <medinamauvecin@gmail.com>
2021-08-10 11:38:46 -0300

    agrega funcionalidad de eliminar las tareas realizadas

--- js/index.js
+++ js/index.js
@@ -5,6 +5,9 @@ $app.innerHTML = `
     <button type="submit" class="btn" name="submit">+</button>
 </form>
 <div class="todo-list">
+</div>
+<div class="buttons oculto">
+<button class="btn btn-delete">Eliminar las tareas completadas</button>
 </div>`;
 
 const $form = document.forms['todo-list'];
@@ -13,12 +16,16 @@ const $btnSubmit = $form.elements.submit;
 
 const $todoList = $app.querySelector('.todo-list');
 const $btnDelete = $app.querySelector('.btn');
+
+const $divButtons = $app.querySelector('.buttons');
+const $btnDeleteAll = $app.querySelector('.btn-delete');
 let allToDo = [];
 
 function manejarEventos() {
   $form.addEventListener('submit', agregarToDo);
   $todoList.addEventListener('click', borrarToDo);
   $todoList.addEventListener('change', checkearToDo);
+  $btnDeleteAll.addEventListener('click', eliminarTareasRealizadas);
 }
 
 function agregarToDo(e) {
@@ -55,6 +62,27 @@ function checkearToDo(e) {
   $tarea.classList.toggle('checked');
 
   allToDo[id].completo = true;
+  manejarBtnDeleteToDos();
+}
+
+function eliminarTareasRealizadas() {
+  let filtro = allToDo.filter((value) => value.completo != true);
+  allToDo = [...filtro];
+  renderizarToDos(allToDo);
+}
+
+function manejarBtnDeleteToDos() {
+  if (allToDo.length === 0) {
+    $divButtons.classList.add('oculto');
+  }
+  for (let i = 0; i < allToDo.length; i++) {
+    if (allToDo[i].completo) {
+      $divButtons.classList.remove('oculto');
+      return;
+    } else {
+      $divButtons.classList.add('oculto');
+    }
+  }
 }
 
 function renderizarToDos(toDo) {
@@ -81,6 +109,7 @@ function renderizarToDos(toDo) {
   });
 
   $todoList.innerHTML = toDoHtml;
+  manejarBtnDeleteToDos();
 }
 
 manejarEventos();
